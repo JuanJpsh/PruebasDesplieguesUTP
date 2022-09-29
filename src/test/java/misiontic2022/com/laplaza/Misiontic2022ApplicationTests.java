ae8af078abcb4abaa846be8f71b1ddcf505ad330
juansolarteh <juanpablosh@unicauca.edu.co>
2022-09-29 08:08:50 -0500

    subida y conf

---
+++ src/test/java/misiontic2022/com/laplaza/Misiontic2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package misiontic2022.com.laplaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Misiontic2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
